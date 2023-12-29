54e40b147bd3c95c828f53e9fe6c9c6abe45007c
rezalfara <reza.alfara07@gmail.com>
2023-12-29 23:28:07 +0700

    commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,8 @@ class Post extends Model
     // protected $fillable = ['title', 'excerpt', 'body'];  //yang boleh di isi
     protected $guarded = ['id']; //guarded = yang tidak boleh di isi
 
+    protected $with = ['category', 'author'];
+
     public function category(){
         return $this->belongsTo(Category::class);
     }
